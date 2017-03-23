efff9bcd8ad3cef72feda8cff37856fe486a73f3
R. Tyler Croy <tyler@monkeypox.org>
2017-03-23 11:44:40 -0700

    Initial commit based on `bundle gem`

---
+++ lib/asciidoctor/jenkins/extensions.rb
@@ -0,0 +1,9 @@
+require "asciidoctor/jenkins/extensions/version"
+
+module Asciidoctor
+  module Jenkins
+    module Extensions
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/asciidoctor/jenkins/extensions/version.rb
@@ -0,0 +1,7 @@
+module Asciidoctor
+  module Jenkins
+    module Extensions
+      VERSION = "0.1.0"
+    end
+  end
+end
